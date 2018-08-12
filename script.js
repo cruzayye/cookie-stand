5050b7b6930658faea0632818f67912f6ac24474
cruzayye <johnnyc010392@gmail.com>
2018-08-11 21:53:04 -0700

    75% there

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 
-
+    //start by creating a func that geneartes a random number 
     var randomGenerator = function(min, max, avgSale){
             var random = Math.floor(Math.random()*(max)) + min;
             var PerHourTotal = random * avgSale;
             return parseInt(PerHourTotal);
         }
+
+        
     var pioneer ={
         minCust: 17,
         maxCust: 88,
         avgSale: 5.2,
         totalCookies: 0,
-        openHours: [ '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm' ],
+        openHours: [ '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm' ],
+        //this adds up the array and prints the total
         totalPrint: function(){
             var total = document.getElementById('pioneer-list');
             
             total.innerHTML += '<li>'+ 'total: ' + pioneer.totalCookies +'<li>'
 
         },
+        //gets teh TOTAL FROM the array, adding each number up
        toHTML: function() {
            var cookiesPerHour  = randomGenerator(pioneer.minCust, pioneer.maxCust, pioneer.avgSale) 
            pioneer.totalCookies += cookiesPerHour;
@@ -24,7 +28,7 @@
            return '<li>' + pioneer.openHours[i] + ': ' + cookiesPerHour +  '</li>';
       },
 
-      
+      //
       pioneerList: function(){
         var list = document.getElementById('pioneer-list');
         for( i = 0; i < pioneer.openHours.length; i++){
@@ -40,43 +44,51 @@
     
     }
   } 
-  pioneer.pioneerList();
-  pioneer.totalPrint();
+    pioneer.pioneerList();
+    pioneer.totalPrint();
            
 
 
 
-
-
-
-/*
     
 
     var waterFront ={
         minCust: 6,
         maxCust: 24,
         avgSale: 1.2,
+        totalCookies: 0,
+
         openHours: [ '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'],
-       toHTML: function() {
-           return '<li>' + waterFront.openHours[i] + ': ' + randomGenerator(waterFront.minCust, waterFront.maxCust, waterFront.avgSale) + '</li>';
-      }
-      
-    } 
-    
-     function waterFrontList(){
-        var list = document.getElementById('waterfront');
-        for( i = 0; i < waterFront.openHours.length; i++){
-            var perHour = (randomGenerator(waterFront.minCust, waterFront.maxCust, waterFront.avgSale));
-            list.innerHTML += waterFront.toHTML();
-            //console.log(pioneer.openHours[i]);
+        totalPrint: function(){
+            var total = document.getElementById('waterfront');
+            total.innerHTML += '<li>'+ 'total: ' + waterFront.totalCookies +'<li>'
             
 
+        },
+        toHTML: function() {
+            var cookiesPerHour  = randomGenerator(waterFront.minCust, waterFront.maxCust, waterFront.avgSale) 
+            waterFront.totalCookies += cookiesPerHour;
+            return '<li>' + waterFront.openHours[i] + ': ' + cookiesPerHour +  '</li>';
+         },
+        waterFrontList:function (){
+            var list = document.getElementById('waterfront');
+            for( i = 0; i < waterFront.openHours.length; i++){
+                var perHour = (randomGenerator(waterFront.minCust, waterFront.maxCust, waterFront.avgSale));
+                list.innerHTML += waterFront.toHTML();
+                //console.log(pioneer.openHours[i]);
+
+                
 
 
 
-        } 
 
-      } waterFrontList();
+            } 
+      
+        }   
+    } 
+    waterFront.waterFrontList(); 
+    waterFront.totalPrint();
+
 
 
 
@@ -84,30 +96,167 @@
         minCust: 11,
         maxCust: 38,
         avgSale: 1.9,
+        totalCookies: 0,
+
         openHours: [ '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'],
-       toHTML: function() {
-           return '<li>' + lloyd.openHours[i] + ': ' + randomGenerator(lloyd.minCust, lloyd.maxCust, lloyd.avgSale) + '</li>';
-      }
-      
-    } 
-    
-     function lloydList(){
-        var list = document.getElementById('lloyd');
-        for( i = 0; i < lloyd.openHours.length; i++){
-            var perHour = (randomGenerator(lloyd.minCust, lloyd.maxCust, lloyd.avgSale));
-            list.innerHTML += lloyd.toHTML();
-            //console.log(pioneer.openHours[i]);
+        totalPrint: function(){
+            var total = document.getElementById('lloyd');
+            total.innerHTML += '<li>'+ 'total: ' + waterFront.totalCookies +'<li>'
             
 
+        },
+        toHTML: function() {
+            var cookiesPerHour  = randomGenerator(lloyd.minCust, lloyd.maxCust, lloyd.avgSale) 
+            lloyd.totalCookies += cookiesPerHour;
+            return '<li>' + lloyd.openHours[i] + ': ' + cookiesPerHour +  '</li>';
+         },
+         lloydList:function (){
+            var list = document.getElementById('lloyd');
+            for( i = 0; i < lloyd.openHours.length; i++){
+                var perHour = (randomGenerator(lloyd.minCust, lloyd.maxCust, lloyd.avgSale));
+                list.innerHTML += lloyd.toHTML();
+                //console.log(pioneer.openHours[i]);
+
+                
 
 
 
-        } 
 
-      } lloydList();
+            } 
+      
+        }   
+    } 
+    lloyd.lloydList(); 
+    lloyd.totalPrint();
 
 
-*/
+
+    var alberta ={
+        minCust: 11,
+        maxCust: 38,
+        avgSale: 1.9,
+        totalCookies: 0,
+
+        openHours: [ '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'],
+        totalPrint: function(){
+            var total = document.getElementById('alberta');
+            total.innerHTML += '<li>'+ 'total: ' + alberta.totalCookies +'<li>'
+            
+
+        },
+        toHTML: function() {
+            var cookiesPerHour  = randomGenerator(alberta.minCust, alberta.maxCust, alberta.avgSale) 
+            alberta.totalCookies += cookiesPerHour;
+            return '<li>' + alberta.openHours[i] + ': ' + cookiesPerHour +  '</li>';
+         },
+         List:function (){
+            var list = document.getElementById('alberta');
+            for( i = 0; i < alberta.openHours.length; i++){
+                var perHour = (randomGenerator(alberta.minCust, alberta.maxCust, alberta.avgSale));
+                list.innerHTML += alberta.toHTML();
+                //console.log(pioneer.openHours[i]);
+
+                
+
+
+
+
+            } 
+      
+        }   
+    } 
+    alberta.List(); 
+    alberta.totalPrint();
+
+    var tigard ={
+        minCust: 11,
+        maxCust: 38,
+        avgSale: 1.9,
+        totalCookies: 0,
+
+        openHours: [ '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'],
+        totalPrint: function(){
+            var total = document.getElementById('tigard');
+            total.innerHTML += '<li>'+ 'total: ' + tigard.totalCookies +'<li>'
+            
+
+        },
+        //this creats the total and returns it as a list along with the info within the array
+        toHTML: function() {
+            var cookiesPerHour  = randomGenerator(tigard.minCust, tigard.maxCust, tigard.avgSale) 
+            tigard.totalCookies += cookiesPerHour;
+            return '<li>' + tigard.openHours[i] + ': ' + cookiesPerHour +  '</li>';
+         },
+         //this takes the above function and actually prints it to the html 6 times by looping through it. 
+         List:function (){
+            var list = document.getElementById('tigard');
+            for( i = 0; i < tigard.openHours.length; i++){
+                list.innerHTML += tigard.toHTML();
+                //console.log(pioneer.openHours[i]);
+
+                
+
+
+
+
+            } 
+      
+        }   
+    } 
+    tigard.List(); 
+    tigard.totalPrint();
+
+    var beaverton ={
+        minCust: 11,
+        maxCust: 38,
+        avgSale: 1.9,
+        totalCookies: 0,
+
+        openHours: [ '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'],
+        totalPrint: function(){
+            var total = document.getElementById('beaverton');
+            total.innerHTML += '<li>'+ 'total: ' + beaverton.totalCookies +'<li>'
+            
+
+        },
+        //this creats the total and returns it as a list along with the info within the array
+        toHTML: function() {
+            var cookiesPerHour  = randomGenerator(beaverton.minCust, beaverton.maxCust, beaverton.avgSale) 
+            beaverton.totalCookies += cookiesPerHour;
+            return '<li>' + beaverton.openHours[i] + ': ' + cookiesPerHour +  '</li>';
+         },
+         //this takes the above function and actually prints it to the html 6 times by looping through it. 
+         List:function (){
+            var list = document.getElementById('beaverton');
+            for( i = 0; i < beaverton.openHours.length; i++){
+                list.innerHTML += beaverton.toHTML();
+                //console.log(pioneer.openHours[i]);
+
+                
+
+
+
+
+            } 
+      
+        }   
+    } 
+    beaverton.List(); 
+    beaverton.totalPrint();
+
+
+
+    
+    
+
+
+     
+
+
+
+
+
+
 
 
 
